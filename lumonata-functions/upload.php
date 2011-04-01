acf02e125dd3f2bfcda018b28ddbfde912f3d7f6
Wahya Biantara <git@arunna.com>
2011-04-01 15:09:10 +0800

    each function has been documented

--- lumonata-functions/upload.php
+++ lumonata-functions/upload.php
@@ -1,4 +1,18 @@
 <?php
+/**
+ * To upload file 
+ *  
+ *     
+ *
+ * @author Wahya Biantara
+ * 
+ * @since alpha
+ * 
+ * @param string $source Source Path
+ * @param string $destination Destination Path
+ *  
+ * @return boolean     
+ */
 function upload($source,$destination){
 	$upload=move_uploaded_file($source,$destination);
 	chmod($destination,0644);
@@ -8,7 +22,22 @@ function upload($source,$destination){
 
 	return false;
 }
-
+/**
+ * To upload image and resize it using the given dimension 
+ *     
+ *
+ * @author Wahya Biantara
+ * 
+ * @since alpha
+ * 
+ * @param string $source Source Path
+ * @param string $destination Destination Path
+ * @param string $file_type mime file type. Alowed file types: image/jpg, image/jpeg,image/pjeg,image/gif, image/png
+ * @param integer $max_width Maximum width of the new resized image
+ * @param integer $max_height Maximum height of the new resized image
+ *  
+ * @return boolean     
+ */
 function upload_resize($source,$destination,$file_type,$max_width,$max_height){
 	$new_width=0;
 	$new_height=0;
@@ -79,6 +108,19 @@ function upload_resize($source,$destination,$file_type,$max_width,$max_height){
 	chmod($destination, 0644);
 	return true;
 }
+
+/**
+ * To remove special character and replace it using "-".  
+ *     
+ *
+ * @author Wahya Biantara
+ * 
+ * @since alpha
+ * 
+ * @param string $char String to be checked and replaced
+ *  
+ * @return string return the clean string     
+ */
 function character_filter($char){
 	$escape_char = array ("\\","/",":","*","?","<",">","`","~","!","@","#","$","%","^","&","(",")","_","+","=","|","}","{","[","]",";","\"","'",",","."," ");
 	
@@ -100,6 +142,19 @@ function character_filter($char){
 	return strtolower($filename.$fileext);
 }
 
+/**
+ * Used when uploading files. To clean the file name if there are some space bar or spechial character   
+ *     
+ *
+ * @author Wahya Biantara
+ * 
+ * @since alpha
+ * 
+ * @param string $file_name File name to be checked
+ * @param boolean $ext Boolean variable. Set TRUE will return the file extension
+ *  
+ * @return string return the clean string     
+ */
 function file_name_filter($file_name,$ext=false){
 	$fileext='';
 	$filename='';
@@ -113,17 +168,56 @@ function file_name_filter($file_name,$ext=false){
 	return generateSefUrl($filename);
 }
 
-//rename filename
+
+/**
+ * Used for file renaming   
+ *     
+ *
+ * @author Wahya Biantara
+ * 
+ * @since alpha
+ * 
+ * @param string $original_file_name The original file name(with extension)
+ * @param string $new_file_name The new file name(without extension)
+ *  
+ * @return string return the new file name complate with the extension     
+ */
 function rename_file($original_file_name,$new_file_name){
 	return $new_file_name.strchr($original_file_name,".");
 }
-//check filesize
-function is_allow_file_size($file_size,$allowed_size=6291456){
+
+/**
+ * Used to define the allowed file size. The defailt allowed file size   
+ *     
+ *
+ * @author Wahya Biantara
+ * 
+ * @since alpha
+ * 
+ * @param integer $file_size the file size
+ * @param integer $allowed_size the limit of allowed file size. Default allowed file size is 2MB/2048KB
+ *  
+ * @return boolean      
+ */
+function is_allow_file_size($file_size,$allowed_size=2048){
 	if($file_size<=$allowed_size || $file_size==0){
 		return true;
 	}
 	return false;
 }
+/**
+ * Used to define which file type are allowed in mime type   
+ *     
+ *
+ * @author Wahya Biantara
+ * 
+ * @since alpha
+ * 
+ * @param string $filetype file upload section type (image,flash,video,music,pdf,doc)to be checked with the file type
+ * @param string $file_checked the file type in mime
+ *  
+ * @return boolean      
+ */
 function is_allow_file_type($filetype,$file_checked){
 	switch($file_checked){
 		case 'image':
@@ -151,7 +245,19 @@ function is_allow_file_type($filetype,$file_checked){
 
 	return false;
 }
-//delete file
+
+/**
+ * Delete file in selected destination   
+ *     
+ *
+ * @author Wahya Biantara
+ * 
+ * @since alpha
+ * 
+ * @param string $destination destination of deleted file
+ *  
+ * @return boolean      
+ */
 function delete_file($destination){
 	if(file_exists($destination)){
 		unlink($destination);
@@ -161,6 +267,22 @@ function delete_file($destination){
 	
 }
 
+/**
+ * To upload image and crop it with the given dimension 
+ *     
+ *
+ * @author Wahya Biantara
+ * 
+ * @since alpha
+ * 
+ * @param string $source Source Path
+ * @param string $destination Destination Path
+ * @param string $file_type mime file type. Alowed file types: image/jpg, image/jpeg,image/pjeg,image/gif, image/png
+ * @param integer $max_width Maximum width of the new resized image
+ * @param integer $max_height Maximum height of the new resized image
+ *  
+ * @return boolean     
+ */
 function upload_crop($source,$destination,$file_type,$max_width,$max_height) {  
 	
 	list($w,$h) = getimagesize($source);
